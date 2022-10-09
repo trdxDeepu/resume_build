19614e5efa3f2cd04c66740f5a3d2d384a332270
trdxlucifer <singhdepu566@gmail.com>
2022-10-09 17:34:11 +0530

    first commit

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const app = express();
 const dbConnect = require("./dbConnect");
 app.use(express.json());
 const userRoute = require("./Routes/userRoute");
-
-const port = 5000
+const port = process.env.PORT || 5000;
+const path = require("path");
 
 app.use("/api/user/", userRoute);
+
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+    });
+  }
 
 app.get('/',(req,res)=> res.send('Hello World'))
 app.listen(port,()=>console.log(`Example app listeamning port ${port}!`))
